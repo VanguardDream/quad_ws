d9ad622ca59c46056f4510c80c649e080c2b18a6
bong <doorebong@dgist.ac.k>
2020-04-02 12:35:42 +0900

    make SAL launch and button work

--- src/quadnake_base/src/simple_joy_receiver.cpp
+++ src/quadnake_base/src/simple_joy_receiver.cpp
@@ -32,19 +32,29 @@ void quad_receiver::joycallback( const sensor_msgs::Joy::ConstPtr& joy_msg )
     quadnake_msgs::Drive sending_msg;
     
     //select gait mode from joy
-    int q_mode = joy_msg->buttons[0];
+    int q_mode = 0; // = joy_msg->buttons[0];
+
+        for(int idx=0 ; idx <= 3; idx++)
+    {
+        if(joy_msg->buttons[idx])
+            q_mode = idx+1;
+    }
 
     //make some value to drive robot
     float fb_linear = joy_msg->axes[1] * 100.0f;
-    float lr_linear = joy_msg->axes[0] * 100.0f;
-    float angular = joy_msg->axes[3] * 100.0f;
+    float lr_linear = joy_msg->axes[0] * -100.0f;
+    float angular = joy_msg->axes[3] * -100.0f;
 
     //make some value to adjust platform
 
 
     //publish msg
+    sending_msg.MODE = (int8_t)q_mode;
     sending_msg.FORWARD_DRIVE = (int8_t)fb_linear;
     sending_msg.SIDE_DRIVE = (int8_t)lr_linear;
+    sending_msg.ANGULAR_DRIVE = (int8_t)angular;
+
+
 
     _pub.publish(sending_msg);
 }
